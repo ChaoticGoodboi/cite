0aa8aa98fe1d743bceaf101df535e366cb32766a
ChaoticGoodboi <theiocaicatxil@gmail.com>
2020-05-31 00:09:05 +0300

    DONE

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -9,18 +9,32 @@ let clickY = new Array();
 
 let clickDrag = new Array();
 
-let paint=false;
+let r = document.getElementById("red");
+let g = document.getElementById("green"); 
+let b = document.getElementById("blue");
+
+
 
 let mouseX;
 let mouseY;
 
 
-let offsetLeft = canvas.parentElement.offsetLeft;
-let offsetTop = canvas.parentElement.offsetTop;
+let offsetLeft = canvas./*parentElement.*/offsetLeft;
+let offsetTop = canvas./*parentElement.*/offsetTop;
+
+let paint = false;
+let color = "white";
+let size = 5;
 
 
 let log = document.getElementById('log');
 
+
+canvas.addEventListener("mouseup", function (e) {
+    log.innerHTML = "Waiting";
+    paint = false;
+    
+});
 
 canvas.addEventListener("mousedown", function (e) {
     //log.innerHTML = "you CLICKEd, bastard!"; //работает
@@ -38,14 +52,14 @@ canvas.addEventListener("mousedown", function (e) {
 canvas.addEventListener("mousemove", function (e) {
     
     if (paint) {
-        log.innerHTML = "You moved!";
-        addClick(e.pageX - this.offsetLeft /*- offsetLeft*/, e.pageY - this.offsetTop /*- offsetTop*/);
+        //log.innerHTML = "You moved!";
+        addClick(e.pageX - this.offsetLeft /*- offsetLeft*/, e.pageY - this.offsetTop/*- offsetTop*/);
         redraw();}
 });
 
 
 function addClick(x, y, dragging) {
-    //log.innerHTML = "AddClick worked";
+    log.innerHTML = x.toString() + ", " + y.toString();
     clickX.push(x);
     clickY.push(y);
     clickDrag.push(dragging);
@@ -53,24 +67,28 @@ function addClick(x, y, dragging) {
 
 function redraw() {
     
-    //context.ClearRect(0, 0, canvas.width, canvas.height);
-    log.innerHTML = "Redrew";
+    
+    //log.innerHTML =  "rgb("+r.value.toString() + ", " + g.value.toString() + ", " + b.value.toString()+")";
+    
 
-    context.strokeStyle = "blue";
+
+    context.strokeStyle = "rgb(" + r.value.toString() + ", " + g.value.toString() + ", " + b.value.toString() + ")";
     context.lineJoin = "round";
-    context.lineWidth = 5;
+    context.lineWidth = size;
 
     for (var i = 0; i < clickX.length; i++) {
         context.beginPath();
         if (clickDrag[i] && i) {
             context.moveTo(clickX[i - 1], clickY[i - 1]);
         } else {
-            context.moveTo(clickX[i] - 1, clickY[i]);
+            context.moveTo(clickX[i]- 1 , clickY[i]);
         }
         context.lineTo(clickX[i], clickY[i]);
         context.closePath();
         context.stroke();
     }
+    context.ClearRect(0, 0, canvas.width, canvas.height);
+    //paint = false;
 }
 
 /*var ClearButton = document.getElementById("clear"); /*TODO - add the button id 
@@ -79,5 +97,11 @@ function Clear() {
     log.innerHTML = "Button Clear Pressed";
     //document.write("yeet");
     context.clearRect(0, 0, canvas.width, canvas.height);
+    clickX = [];
+    clickY = [];
+}
+
+function ChangeColor(colorInput) {
+    this.color = colorInput;
 }
 
